17fc8554272124f33e08e3ef025e47611932006a
Gladys Romero <gladysromero@live.com.ar>
2023-09-13 19:47:22 -0300

    cree las carpetas para los css y js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const colorPicker = document.getElementById("colorPicker");
+const nota = document.querySelector(".nota");
+
+colorElegido.addEventListener("change", () => {
+    const colorElegido = colorPicker.value;
+
+    nota.style.backgroundColor = colorElegido;
+})    
