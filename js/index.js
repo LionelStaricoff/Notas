02197d8706b986b208026fb1d4e8b2d676a24a94
Gladys Romero <gladysromero@live.com.ar>
2023-09-13 21:51:29 -0300

    el postick cambia de color y también la esquina doblada

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const colorPicker = document.getElementById("colorPicker");
-const nota = document.querySelector(".nota");
-
-colorElegido.addEventListener("change", () => {
-    const colorElegido = colorPicker.value;
-
-    nota.style.backgroundColor = colorElegido;
-})    
--- js/nota.js
+++ js/nota.js
@@ -1,8 +1,10 @@
 const colorPicker = document.getElementById("colorPicker");
 const nota = document.querySelector(".nota");
+const triangle = document.querySelector(".triangle");
 
-colorElegido.addEventListener("change", () => {
+colorPicker.addEventListener("change", () => {
     const colorElegido = colorPicker.value;
 
     nota.style.backgroundColor = colorElegido;
+    triangle.style.borderLeftColor = colorElegido;
 })
