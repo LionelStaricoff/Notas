4941e516a8f0da82f07c02c7c943eb32f234e22b
Gladys Romero <gladysromero@live.com.ar>
2023-09-14 18:33:01 -0300

    comencé a ver el notaReducida.js

---
+++ js/notaReducida.js
@@ -0,0 +1,25 @@
+const botonEliminar = document.querySelector(".notaReducida .boton-eliminar");
+const botonEditar = document.querySelector(".notaReducida .boton-editar");
+const botonMover = document.querySelector(".notaReducida .boton-mover");
+const notaReducida = document.querySelector(".notaReducida");
+const cambioDeColumna = document.querySelector(".cambioDeColumna");
+
+botonEliminar.addEventListener("click", () => {
+    alert("boton-eliminar")
+    
+});
+
+botonEditar.addEventListener("click", ()=>{
+    alert("boton-editar")
+
+});
+
+botonMover.addEventListener("click", () => {
+    /*
+    if (cambioDeColumna.style.display === "none") {
+        cambioDeColumna.style.display = "block";
+    } else {
+        cambioDeColumna.style.display = "none";
+    }
+    */
+}); 
