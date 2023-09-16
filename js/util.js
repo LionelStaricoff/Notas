c620f86fd5509bbb0ae422106169a29d9f223eca
lionel staricoff <staricofflionel@hotmail.com>
2023-09-16 16:52:03 -0500

    funcion para agregar notas al front con un objeto util.js

---
+++ js/util.js
@@ -0,0 +1,8 @@
+const util = {
+    btnCrearNota: function(){
+        const columnas = document.querySelectorAll('.columnaCuadro');
+        const nota = new Nota();
+        nota.agregarAlFront();
+
+    }
+}
