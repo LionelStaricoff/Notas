ee56c55fc64ceae4381854d18f211283d007a528
lionel staricoff <staricofflionel@hotmail.com>
2023-09-17 21:59:30 -0500

    aplicando funcionalidad del local storage

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
-button.addEventListener('click', util.btnCrearNota);
+//agregar notas
+buttons[2].addEventListener('click', util.btnCrearNota);
   
+//guardar todas las notas
+buttons[0].addEventListener('click', util.btnGuardar);
+ 
+//cargrar todas las notas
+buttons[1].addEventListener('click', util.btnCargar);
+
+
+
+
