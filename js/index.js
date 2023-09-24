b6cc9233fd9799262292b74fcecef7748ee2795d
lionel staricoff <staricofflionel@hotmail.com>
2023-09-24 09:16:01 -0500

    se cambian las bariables individuales por la lista de botones

--- js/index.js
+++ js/index.js
@@ -1,26 +1,9 @@
-/*
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('li');
 
-//agregar notas
-buttons[2].addEventListener('click', util.btnCrearNota);
 
-//guardar todas las notas
-buttons[0].addEventListener('click', util.btnGuardar);
-
-//cargrar todas las notas
-buttons[1].addEventListener('click', util.btnCargar);
-*/
-
-const crearNotaMenuItem = document.getElementById('crearNota');
-const recuperarNotaMenuItem = document.getElementById('recuperarNota');
-const guardarTableroMenuItem = document.getElementById('guardarTablero');
-//faltan hacer la funciones de los de abajo:
-const exportarTableroMenuItem = document.getElementById('exportarTablero');
-const vaciarTableroMenuItem = document.getElementById('vaciarTablero');
-
-crearNotaMenuItem.addEventListener('click', util.btnCrearNota);
-recuperarNotaMenuItem.addEventListener('click', util.btnCargar);
-guardarTableroMenuItem.addEventListener('click', util.btnGuardar);
+buttons[1].addEventListener('click', util.btnCrearNota);
+buttons[2].addEventListener('click', util.btnCargar);
+buttons[3].addEventListener('click', util.btnGuardar);
 //estos son los eventos para las funciones que faltan:
-exportarTableroMenuItem.addEventListener('click', util.exportarTablero);
-vaciarTableroMenuItem.addEventListener('click', util.vaciarTablero);
+buttons[4].addEventListener('click', util.exportarTablero);
+buttons[5].addEventListener('click', util.vaciarTablero);
