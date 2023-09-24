256bd49c7e69721a65a6bb0fcec1efe1c09da456
Gladys Romero <111000044+Gladys077@users.noreply.github.com>
2023-09-24 19:40:18 -0300

    modifiqué el nombre de un item del menú

--- js/Nota.js
+++ js/Nota.js
@@ -149,7 +149,7 @@ class Nota {
     eliminar(nota, btnX) {
         btnX.addEventListener('click', () => {
             // modificar el prompt
-            const respuesta = confirm("Seguro que decesa eliminarlo?");
+            const respuesta = confirm("¿Seguro que deseas eliminarlo?");
             if(respuesta==1){
             const padre = nota.parentNode;
             padre.removeChild(nota);
